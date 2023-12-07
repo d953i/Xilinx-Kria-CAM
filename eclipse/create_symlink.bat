0ccfd72b12193710fb8323d34840c77a01708780
Dmitriy Ivashin <6982205+d953i@users.noreply.github.com>
2023-12-07 17:54:47 -0500

    Update create_symlink.bat

--- eclipse/create_symlink.bat
+++ eclipse/create_symlink.bat
@@ -2,7 +2,7 @@
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
 
 set source=D:\Projects\_github\Xilinx-Kria-CAM
-set target=D:\Projects\Software-MinGW\Xilinx-Kria-CAM
+set target=D:\Projects\Software-MinGW\Xilinx_Kria_CAM
 set exclude=DoNotLinkThisDirectory
 
 forfiles /P "%source%" /C "cmd /c if @isdir==TRUE (if not @file==\"%exclude%\" mklink /d \"%target%\@file\" @path ) else ( mklink \"%target%\@file\" @path )"
